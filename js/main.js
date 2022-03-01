21a7c1cdd515a8fb8c5d4cc23d2b28f975819c43
rezainiet <masudrezaog@gmail.com>
2022-03-02 03:39:47 +0600

    product details UI part added with JS

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ const searchPhone = () => {
     fetch(url)
         .then(res => res.json())
         .then(data => displayPhone(data.data));
+
+    textValue.value = '';
 }
 
 const displayPhone = (phones) => {
@@ -28,6 +30,34 @@ const phoneDetails = (detailsId) => {
     const url = `https://openapi.programming-hero.com/api/phone/${detailsId}`
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data.data.brand));
-    // console.log(detailsId);
+        .then(data => showPhonedetails(data.data))
+        .then(console.log(url));
+}
+
+const showPhonedetails = (phoneSLug) => {
+    const phoneDetailsContainer = document.getElementById('phone-details-container');
+    const div = document.createElement('div');
+    div.innerHTML = `<div class="phone-details-container">
+            <div class="image-container">
+                <img src="./images/1.jpg" alt="">
+            </div>
+            <div class="details-container">
+                <h2>Name: ${phoneSLug.name}.</h2>
+                <h2>Brand: ${phoneSLug.brand}.</h2>
+                <h3>Release Date: ${phoneSLug.releaseDate}.</h3>
+                <h3 style="text-decoration: underline;">Main Features:</h3>
+                <div class="main-features-wrapper">
+                    <p>Storage: ${phoneSLug.mainFeatures.storage}.</p>
+                    <p>Display Size: ${phoneSLug.mainFeatures.displaySize}. </p>
+                    <p>Chipset: ${phoneSLug.mainFeatures.chipSet}. </p>
+                    <p>Memory: ${phoneSLug.mainFeatures.memory}. </p>
+                    <h4 style="text-decoration: underline;">Sensors:</h4>
+                    <p>${phoneSLug.mainFeatures.sensor[0]}</p>
+                    <p>Sensor 1</p>
+                    <p>Sensor 1</p>
+                    <p>Sensor 1</p>
+                </div>
+            </div>
+        </div>`
+    phoneDetailsContainer.appendChild(div);
 }
